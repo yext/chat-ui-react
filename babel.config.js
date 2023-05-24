0b6c176692e7dfff7685288af09c6761080e7a5c
Yen Truong <ytruong@yext.com>
2023-05-24 14:01:14 -0400

    target node

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    "@babel/preset-env",
+    ['@babel/preset-env', {targets: {node: 'current'}}],
     // enables a new JSX transform introduced in React 17 that uses the
     // React runtime to generate necessary code for JSX expressions.
     ["@babel/preset-react", { runtime: "automatic" }],
