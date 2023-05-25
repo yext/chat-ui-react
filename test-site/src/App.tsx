f7baee796c052ebc8395e053ab1a8b8d9cd16365
Yen Truong <36055303+yen-tt@users.noreply.github.com>
2023-05-25 16:47:23 -0400

    scaffold repo infrastructure (#1)
    this PR setup the basic infrastructure for the repo:
    - setup npm package, with build command to spit out `lib` bundle with types
      - currently compile a dummy `Button` component. Will be deleted once we add actual components
    - setup jest test environment
    - setup a React test site that import components from the library
    - add linting
    
    J=CLIP-156
    TEST=manual&auto
    
    see that bundle are built as expected
    jest command works
    lint command works
    test site successfully render Button component

---
+++ test-site/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@yext/chat-ui-react";
+
+function App() {
+  return (
+    <div className="App">
+      hello world
+      <Button />
+    </div>
+  );
+}
+
+export default App;
