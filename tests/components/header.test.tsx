6d0ac5359427b20e7e154349f8d01617f101e2e1
Noah Siskind <nsiskind@yext.com>
2023-05-31 10:38:18 -0400

    improve tests, use add utils
    Change-Id: Ib1f44364c265b1c15c013ff9c6a7d69ab9a10646

--- tests/components/header.test.tsx
+++ tests/components/header.test.tsx
@@ -1,6 +1,7 @@
 import { render, fireEvent} from '@testing-library/react';
 import { ChatHeader } from "../../src/components";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { useChatActions, ChatHeadless } from '@yext/chat-headless-react';
 import {RecursivePartial} from '../__utils__/mocks'
 
@@ -24,21 +25,33 @@ describe('refresh button', () => {
   beforeEach(()=>mockChatActions(mockedHeadless));
 =======
 import { useChatActions } from '@yext/chat-headless-react';
+=======
+import { useChatActions, ChatHeadless } from '@yext/chat-headless-react';
+import {RecursivePartial} from '../__utils__/mocks'
+>>>>>>> 455b72f (improve tests, use add utils)
 
-  jest.mock('@yext/chat-headless-react', () => {
+export function mockChatActions(
+  customHeadless: RecursivePartial<ChatHeadless>
+): jest.SpyInstance<typeof useChatActions, unknown[]> {
+  return jest
+    .spyOn(require('@yext/chat-headless-react'), 'useChatActions')
+    .mockImplementation(() => customHeadless as ChatHeadless);
+}
 
-    // Mock the headless object
-    const mockedHeadless = {
-      restartConversation: jest.fn(),
-      getNextMessage: jest.fn(),
-    };
 
-   return {useChatActions: () => mockedHeadless};
-  });
-  
+// Mock the headless object
+const mockedHeadless = {
+  restartConversation: jest.fn(),
+  getNextMessage: jest.fn(),
+};
 
 describe('refresh button', () => {
+<<<<<<< HEAD
 >>>>>>> c9c1689 (Add Header component)
+=======
+
+  beforeEach(()=>mockChatActions(mockedHeadless));
+>>>>>>> 455b72f (improve tests, use add utils)
   it('should call chat headless functions', () => {
 
     const { container } = render(
@@ -48,9 +61,13 @@ describe('refresh button', () => {
       />
     )
 <<<<<<< HEAD
+<<<<<<< HEAD
     expect(useChatActions).toHaveBeenCalledTimes(1);
 =======
 >>>>>>> c9c1689 (Add Header component)
+=======
+    expect(useChatActions).toHaveBeenCalledTimes(1);
+>>>>>>> 455b72f (improve tests, use add utils)
 
     const buttonElement = container.querySelector('button');
     expect(buttonElement).not.toBeNull()
@@ -68,10 +85,15 @@ describe('refresh button', () => {
       title="Clippy's Chatbot" 
     />)
 <<<<<<< HEAD
+<<<<<<< HEAD
     
     expect(useChatActions).toHaveBeenCalledTimes(1);
 =======
 >>>>>>> c9c1689 (Add Header component)
+=======
+    
+    expect(useChatActions).toHaveBeenCalledTimes(1);
+>>>>>>> 455b72f (improve tests, use add utils)
 
     const buttonElement = container.querySelector('button');
     expect(buttonElement).toBeNull()
