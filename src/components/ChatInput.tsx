ddda8380843ad53f053e320b1d80bb332e33783d
Yen Truong <ytruong@yext.com>
2023-06-16 16:06:55 -0400

    send button position qa

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -23,9 +23,9 @@ const builtInCssClasses: ChatInputCssClasses = withStylelessCssClasses(
   {
     container: "w-full h-fit flex flex-row relative @container",
     textArea:
-      "w-full p-4 pr-10 border border-slate-300 disabled:bg-slate-50 rounded-3xl resize-none text-[13px] @[480px]:text-base",
+      "w-full p-4 pr-12 border border-slate-300 disabled:bg-slate-50 rounded-3xl resize-none text-[13px] @[480px]:text-base",
     sendButton:
-      "rounded-full p-1.5 w-8 h-8 stroke-2 text-white bg-blue-600 disabled:bg-slate-100 hover:bg-blue-800 active:scale-90 transition-all absolute right-7 bottom-2.5 @[480px]:bottom-3.5",
+      "rounded-full p-1.5 w-8 h-8 stroke-2 text-white bg-blue-600 disabled:bg-slate-100 hover:bg-blue-800 active:scale-90 transition-all absolute right-4 bottom-2.5 @[480px]:bottom-3.5",
   }
 );
 
